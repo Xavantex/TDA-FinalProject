e4b7815a89300114d7f69269c5a097c907cd626b
eerixav <xavante.erickson@ericsson.com>
2023-11-07 21:01:44 +0100

    toy random sampling and H0-H2

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+from pathlib import Path
+
+with open(Path("README.md"), "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="topology",
+    version="0.1",
+    author="xavante",
+    author_email="xavante.erickson@gmail.com",
+    description="topology course",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    #entry_points = {
+    #    'console_scripts': ['convert=convert.convert:main'],
+    #},
+    packages=['stablerank'],#setuptools.find_packages(where='src', exclude=["main"]),
+    package_dir={'stablerank': 'stablerank'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'wheel==0.41.2',
+        'Cython==3.0.2',
+        'Ripser==0.6.4',
+        'pandas==2.0.3',
+        'ipywidgets==8.1.1',
+        'matplotlib==3.7.3',
+    ]
+)
--- src/VisualizeData.py
+++ src/VisualizeData.py
@@ -5,9 +5,11 @@ plt.style.use('ggplot')
 
 from scipy.spatial.distance import cdist, pdist, squareform
 import stablerank.srank as sr
-from numpy import shape, min, max, mean, where
+from numpy import shape, min, max, mean, where, argsort, zeros
+from numpy.random import choice
 
 from ripser import ripser as rp
+from persim import plot_diagrams
 
 sDir = Path(__file__).parent
 
@@ -16,12 +18,28 @@ urbPath = sDir / Path('./data/urban2012-01-08/velodyne_sync/1326036605234924.bin
 urb = loadUrban(urbPath)
 #urb = urb[:20000, :]
 # Center the points w.r.t. average
-urb = urb - urb.mean(axis=0)
+#urb = urb - urb.mean(axis=0)
+
 # Radii of 4 meter from center
-surb = urb[:,0]**2 * urb[:,1]**2 > 16
-urb = urb[surb]
+#surb = urb[:,0]**2 * urb[:,1]**2 < 4
+#urb = urb[surb]
+
+#print("urban", shape(urb))
 #print(urb.max(axis=0))
 #print(urb.min(axis=0))
+
+# choose random point, sort by the closest points and choose the 100 closest points.
+nump = 3
+npoint = 1000
+
+rurb = choice(shape(urb)[0], nump)
+
+curb = zeros((nump, npoint, 3))
+
+for idx, sample in enumerate(rurb):
+    tmp = urb[argsort(cdist(urb, urb[None, sample]).squeeze())]
+    curb[idx] = tmp[:npoint]
+
 
 
 natPath = sDir / Path('./data/outdoorBox_met/box_met_000/box_met_000.xyz')
@@ -29,16 +47,29 @@ natPath = sDir / Path('./data/outdoorBox_met/box_met_000/box_met_000.xyz')
 nat = loadNatural(natPath)
 #nat = nat[:20000, :]
 # Center the points w.r.t. average
-nat = nat - nat.mean(axis=0)
+#nat = nat - nat.mean(axis=0)
+
 # Radii of 4 meter from center
-snat = nat[:,0]**2 * nat[:,1]**2 > 16
-nat = nat[snat]
+#snat = nat[:,0]**2 * nat[:,1]**2 < 4
+#nat = nat[snat]
+
+#print("natural", shape(nat))
 #print(nat.max(axis=0))
 #print(nat.min(axis=0))
 
+# choose random point, sort by the closest points and choose the 100 closest points.
 
-#fig = plt.figure()
-#ax = fig.add_subplot(111)
+rnat = choice(shape(nat)[0], nump)
+
+cnat = zeros((nump, npoint, 3))
+
+for idx, sample in enumerate(rnat):
+    tmp = nat[argsort(cdist(nat, nat[None, sample]).squeeze())]
+    cnat[idx] = tmp[:npoint]
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
 #ax = fig.add_subplot(projection='3d')
 
 #ax.scatter(urb[:,0], urb[:,1], urb[:,2], marker='o')
@@ -52,34 +83,69 @@ nat = nat[snat]
 
 #DURB = sr.Distance(squareform(pdist(urb, metric="euclidean")))
 #DNAT = sr.Distance(squareform(pdist(nat, metric="euclidean")))
-DURB = squareform(pdist(urb, metric="euclidean"))
-#DNAT = squareform(pdist(nat, metric="euclidean"))
+curbdist = []
+for idx, cp in enumerate(curb):
+    curbdist.append(sr.Distance(squareform(pdist(cp, metric="euclidean"))))
+
+cnatdist = []
+for idx, cp in enumerate(cnat):
+    cnatdist.append(sr.Distance(squareform(pdist(cp, metric="euclidean"))))
 
 
-diagrams = rp(DURB, maxdim=2, thresh= 4, distance_matrix=True)['dgms']
-print(diagrams)
+#diagrams = rp(curbdist[0], maxdim=2, thresh= 4, distance_matrix=True)['dgms']
+for idx, cp in enumerate(curbdist):
+    #plot_diagrams(rp(cp, maxdim=2, distance_matrix=True)['dgms'], show=True)
 
-exit(0)
+    maxdim = 2
+    burb = cp.get_bc(maxdim=maxdim)
 
-clustering_method = "complete"
+    furb = sr.bc_to_sr(burb, degree="H0")
 
-maxdim = 1
-bnat = DNAT.get_bc(maxdim=maxdim)
-burb = DURB.get_bc(maxdim=maxdim)
+    purb = furb.plot()
+    purb[0].set_label(f'Urban {idx} H0')
 
-#fnat0 = sr.bc_to_sr(bnat, degree="H0")
-#furb0 = sr.bc_to_sr(burb, degree="H0")
-fnat1 = sr.bc_to_sr(bnat, degree="H1")
-furb1 = sr.bc_to_sr(burb, degree="H1")
+    #furb = sr.bc_to_sr(burb, degree="H1")
 
-pnat = fnat1.plot(label='Natural')
-purb = furb1.plot(label='Urban')
+    #purb = furb.plot(label=f'Urban {idx} H1')
+    #purb[0].set_label(f'Urban {idx} H1')
 
-ax.add_line(pnat[0])
-ax.add_line(purb[0])
+
+    #furb = sr.bc_to_sr(burb, degree="H2")
+
+    #purb = furb.plot(label=f'Urban {idx} H2')
+    #purb[0].set_label(f'Urban {idx} H2')
+
+
+for idx, cp in enumerate(cnatdist):
+    #plot_diagrams(rp(cp, maxdim=2, distance_matrix=True)['dgms'], show=True)
+
+    maxdim = 2
+    bnat = cp.get_bc(maxdim=maxdim)
+
+    fnat = sr.bc_to_sr(bnat, degree="H0")
+
+    pnat = fnat.plot()
+    pnat[0].set_label(f'Natural {idx} H0')
+
+
+    #fnat = sr.bc_to_sr(bnat, degree="H1")
+
+    #pnat = fnat.plot(label=f'Natural {idx} H1')
+    #pnat[0].set_label(f'Natural {idx} H1')
+
+
+    #fnat = sr.bc_to_sr(bnat, degree="H2")
+
+    #pnat = fnat.plot(label=f'Natural {idx} H2')
+    #pnat[0].set_label(f'Natural {idx} H2')
+
+
+fig.legend()
 
 plt.show()
 
+
+#clustering_method = "complete"
 #fnat = DNAT.get_h0sr(clustering_method=clustering_method)
 
 #fnat.plot()
